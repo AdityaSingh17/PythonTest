5209a87f6e9217191b5e90587555e67ac879db58
AdityaSingh17 <asaditya140@gmail.com>
2019-04-20 21:40:40 +0530

    Solution to problem 2

---
+++ Solutions/2. oddeven.py
@@ -0,0 +1,9 @@
+"""
+To get a number from user as input and determine if it is even or not.
+"""
+x = int(input("Please enter a number : "))
+
+if x%2==0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
