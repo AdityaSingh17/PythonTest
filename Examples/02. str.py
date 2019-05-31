fcc6b799f983fa4d4e5a0afc809155cd2c561b02
AdityaSingh17 <asaditya140@gmail.com>
2019-05-31 14:28:02 +0530

    made changes

--- Examples/02. str.py
+++ Examples/02. str.py
@@ -10,7 +10,10 @@ print("In lower case "+x.lower())
 print("Character "+x[1])
 print("Range of charater "+x[2:5]) #2 and 5 not inlucded 
 print("To remove white space from front and end  "+x.strip()) #removes white space at beginning and end
+#lstrip() and rstrip() can be used to remove spaces from left or right of the string.
 a=len(x)
 print("Length ="+str(a)) #since length is an int, it has to be typecasted to string to print!
 print("To replace "+x.replace('H','K'))
 print("To slipt the string based on some character"+str(x.split(','))) #returns ['Hello','World'], has to be typecasted to str
+pos = x.find('a')                           #To find the index of a character.
+print(pos)
