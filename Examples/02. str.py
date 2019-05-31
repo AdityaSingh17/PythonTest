aef844860f3de83a176e85a5f275d4dbac475701
AdityaSingh17 <asaditya140@gmail.com>
2019-05-31 14:31:28 +0530

    changes made

--- Examples/02. str.py
+++ Examples/02. str.py
@@ -7,8 +7,9 @@ x=input()
 print("The string is "+x)
 print("In Upper case "+x.upper())
 print("In lower case "+x.lower())
+print("The capitalized string is",x.capitalize())       #used to capitalize only the first letter. #The ',' creates a space!
 print("Character "+x[1])
-print("Range of charater "+x[2:5]) #2 and 5 not inlucded 
+print("Range of charater "+x[2:5]) #5 not inlucded 
 print("To remove white space from front and end  "+x.strip()) #removes white space at beginning and end
 #lstrip() and rstrip() can be used to remove spaces from left or right of the string.
 a=len(x)
